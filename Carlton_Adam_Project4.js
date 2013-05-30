4576da1e90e9355acb6582e9000937c0d35a1db0
b50789 <acarlton@fullsail.edu>
2013-05-30 11:01:19 -0500

    start of Week4 Project 4

---
+++ Carlton_Adam_Project4.js
@@ -0,0 +1,62 @@
+//alert("JavaScript works!");
+// Adam Carlton
+// Project IV
+// SDI -- 1305
+// Project IV (week 4)
+
+// Start
+
+var myLib = function () {
+    
+    //Phone Number Validation
+    var phoneNum = Function(number){
+	var numberCheck = isNaN(number[0,1,2,4,5,6,8,9,10,11]);
+	
+	if (numberCheck === false && number[3,7]=== "-"){
+	    return true;{
+	    //code
+	} esle {
+	    return false;
+	}
+	
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
